1514115d9ad3306c2aaad262344192d4d9d8c18d
gerlix-server <gerlixtechnology@mail.com>
2022-07-08 16:56:32 -0500

    Act

--- server.js
+++ server.js
@@ -62,13 +62,9 @@ products(app, upload);
 //	console.log('Aplicacion de Nodejs ' + port + ' Iniciada...');
 //});
 
-server.listen(
-	port,
-	'54.235.132.133' ||
-		function () {
-			console.log('Aplicacion de Nodejs ' + port + ' Iniciada...');
-		}
-);
+server.listen(port, '54.235.132.133' || 'localhost', function () {
+	console.log('Aplicacion de Nodejs ' + port + ' Iniciada...');
+});
 
 // ERROR HANDLER
 
